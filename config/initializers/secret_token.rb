9d88184ef0f3846d922f399fbd3074366d709d02
Dean Dieker <ddieker@gmail.com>
2013-04-27 13:48:38 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tvnexthack::Application.config.secret_token = '99a60d296d5ab38d259a55cc5a7dcffb11b3a884691b0112eb6696f0322e352c29d49e128438187fa7af6eb8966339088fbbb5ade9bd02a8949bbb2abdbb321f'
