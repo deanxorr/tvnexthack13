4e3705df3a914bc95fbd6d37e5b9772517fa2953
JP <bonnerjp@gmail.com>
2013-04-27 18:39:05 -0400

    restructured initializers to properly call heroku env vars

--- config/initializers/get_glue.rb
+++ config/initializers/get_glue.rb
@@ -1,3 +1,6 @@
-require "#{Rails.root}/settings" unless Rails.env.production?
-
+if Rails.env.production?
+	Tvnexthack::Application.config.get_glue_token = ENV['GET_GLUE_TOKEN']
+else
+	require "#{Rails.root}/settings"
 Tvnexthack::Application.config.get_glue_token = GET_GLUE_TOKEN
+end
--- config/initializers/tms.rb
+++ config/initializers/tms.rb
@@ -1,3 +1,6 @@
-require "#{Rails.root}/settings" unless Rails.env.production?
-
-Tvnexthack::Application.config.tms_api_key = TMS_API_KEY
+if Rails.env.production?
+	Tvnexthack::Application.config.tms_api_key = ENV['TMS_API_KEY']
+else
+	require "#{Rails.root}/settings"
+	Tvnexthack::Application.config.tms_api_key = TMS_API_KEY
+end
