81fffead60a0b41db373b8c4d16b5bab4541f3a1
JP <bonnerjp@gmail.com>
2013-04-27 18:29:37 -0400

    added conditional requirements in initializers

--- config/initializers/get_glue.rb
+++ config/initializers/get_glue.rb
@@ -1,3 +1,3 @@
-require "#{Rails.root}/settings"
+require "#{Rails.root}/settings" unless Rails.env.production?
 
 Tvnexthack::Application.config.get_glue_token = GET_GLUE_TOKEN
--- config/initializers/tms.rb
+++ config/initializers/tms.rb
@@ -1,3 +1,3 @@
-require "#{Rails.root}/settings"
+require "#{Rails.root}/settings" unless Rails.env.production?
 
 Tvnexthack::Application.config.tms_api_key = TMS_API_KEY
